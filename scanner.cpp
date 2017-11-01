48b2cff4e72fa1fef2649a86deb46e3586e98a96
Aaron <abrunette@users.noreply.github.com>
2017-10-31 17:22:03 -0700

    Update scanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -2,92 +2,125 @@
 #include<fstream>
 #include<string>
 #include<cstdlib>
+#include<vector>
 using namespace std;
 
+vector <string> reservedWords;	//vector holds reserved words
+vector <string> reservedTypes;	//hold reserved word types
+
 //=====================================================
-// File scanner.cpp written by: Group Number: ** 
+// File scanner.cpp written by: Group Number: 3 
 //=====================================================
 
-// ** MYTOKEN DFA to be replaced by the WORD DFA
-// ** Done by:
-// ** RE:
-bool mytoken(string s)
-{
-  int state = 0;
-  int charpos = 0;
-
-  while (s[charpos] != '\0') 
-    {
-      if (state == 0 && s[charpos] == 'a')
-      state = 1;
-      else
-      if (state == 1 && s[charpos] == 'b')
-      state = 2;
-      else
-      if (state == 2 && s[charpos] == 'b')
-      state = 2;
-      else
-	  return(false);
-      charpos++;
-    }//end of while
-
-  // where did I end up????
-  if (state == 2) return(true);  // end in a final state
-   else return(false);
-}
-
-// ** Add the PERIOD DFA here
-// ** Done by:
-
-
-// ** Update the tokentype to be WORD1, WORD2, PERIOD and ERROR.
+//Enumerated token types
 typedef enum tokentype {ERROR, WORD1, WORD2, PERIOD};
 
-// ** Need the lexicon to be set up here (to be used in Part C)
-// ** Need the reservedwords list to be set up here
-// ** Do not require any file input for these.
-// ** a.out should work without any additional files.
+// Period found, returns token
+// ** Done by: Aaron & Erik
+bool period(tokentype& a)
+{
+	tokentype = PERIOD;	//sets token
+	return;
+}
+
+/******************************************************/
+
+//boolean DFA functions
+bool stateZero(&string w)
+{
+	switch(w)
+	{
+		case s:
+			stateEight();
+		case m: case n: case k: case b: case h: case g: case p: case f:
+			stateTen();
+		case y:
+			stateEleven();
+		case a: case i: case u: case e: case o:
+			stateTwelve();
+		case t:
+			stateTwo();
+		case r: case k: case o: case m: case n: case p: case h:
+			stateFour():
+		case d:
+			stateSix();
+		case s:
+			stateEight();	
+	}
+}
+
+/******************************************************/
 
 // Scanner processes only one word each time it is called
-// ** Done by: 
+// ** Done by: Aaron & Erik
 int scanner(tokentype& a, string& w)
 {
-
-  // ** Grab the next word from the file
-
-  /* 
-  2. Call the token functions one after another (if-then-else)
-     And generate a lexical error if both DFAs failed.
-     Let the token_type be ERROR in that case.
-  3. Make sure WORDs are checked against the reservedwords list
-     If not reserved, token_type is WORD1 or WORD2.
-  4. Return the token type & string  (pass by reference)
-  */
-
+	bool result = true;	//default, word is assumed valid
+	
+     		if(w[0] == ('a' || 'i' || 'u' || 'e' || 'o'))
+		{
+			result = statetwelve(w);
+		}
+		else if(w[0] == ('b' || 'g' || 'h' || 'j' || 'k' || 'm' || 'n' || 'p' || 'r'))
+		{
+			result = stateZero(w);
+		}
+		else if(w[0] == 't')
+		{
+		
+		}
+		else if(w[0] == 'c')
+		{}
+		else if(w[0] == 'd')
+		{}
+		else if(w[0] == 's')
+		{}
+		else if(w[0] == 'z')
+		{}
+		else if(w[0] == 'w')
+		{}
+		else if(w[0] == 'y')
+		{}
+		else if(w[0] == '.')
+			return period(a); 
+		else
+		{
+			a = ERROR;
+			return;
+		}	
+	if(result == false)
+	{
+		tokentype = ERROR;
+		return;
+	}
+	else if(tokem)	//valid
+	{
+		tokentype = WORD1;
+		result = dictionary(tokentype a, string w);
+	}
 }//the end
 
-
+//
+//Done by: Aaron & Erik
+bool dictionary(tokentype &a, string w)
+{
+	for(int count = 0; count <= reservedWord.length(); count++)
+	{
+		if(reservedWord[count] == w)
+		{
+			tokentype = reservedType[count];
+			return;
+		}
+	}
+}
 
 // The test driver to call the scanner repeatedly  
-// ** Done by:  Aaron Brunette**
+// ** Done by:  Aaron & Erik
 int main()
 {
-  tokentype thetype;
-  string theword; 
-
-  /*
-1. get the input file name from the user
-2. open the input file which contains a story written in Japanese (fin.open).
-3. call Scanner repeatedly until the EOF marker is read, and
-     each time cout the returned results
-     e.g. STRING TOKEN-TYPE
-          =====  ===========
-          watashi PRONOUN  (from the first call)
-          wa      SUBJECT  (from the second call)
-          gakkou  WORD1
-          etc.
-  */
-
+  	tokentype thetype;
+  	string theword; 
+	
 	string inputfile;
 	printf("Please enter the file name to be scanned: ");
 	getline(cin, inputfile);
@@ -95,20 +128,29 @@ int main()
 	ofstream toRead;
 	toRead.open(inputfile.data());
 	
-  while (true)
-    {
-      scanner(thetype, theword);  // call the scanner
+	string reserved;
+	ofstream dictionary;
+	dictionary.open()
+	
+	while(dictionary)	//fill reserved word vector
+	{
+		string rWord; 
+		string rType;
+		dictionary >> rWord >> rType;
+		
+		reservedWord.push_back(rWord);
+		reservedType.push_back(rType);
+	}
+		
+	dictionary.close();
 
-       cout << "Type is:" << thetype << endl;
-       cout << "Word is:" << theword << endl;   
+	while (toRead)
+	{
+		toRead >> theword;
+	      	scanner(thetype, theword);  // call the scanner
 
-       // ** display the actual type instead of a number
-
-    }
-
-  // ** close the input file
+	       	cout << "Type is:" << thetype << endl;
+	       	cout << "Word is:" << theword << endl;   
+    	}
 	toRead.close();
 }// end
-
-
-
