936c57a1c2d017788469d5c31652c6e4de8e09cf
Aaron <abrunette@users.noreply.github.com>
2017-11-21 14:03:27 -0800

    Add files via upload

--- parser.cpp
+++ parser.cpp
@@ -2,63 +2,76 @@
 #include <stdio.h>
 #include <fstream>
 #include <string>
-#include <stack>	//grammar stack	//maybe make this vector
+#include <stack>	//grammar stack //maybe make this vector
 #include <array>	//parsing table?
 using namespace std;
 
-//Global token vars
-enum token_type{ 	//valid token type creation
-	PERIOD, EOFM, VERB, VERBNEG, VERBPASTNEG, 
-	IS, WAS, OBJECT, SUBJECT, DESTINATION,
-	PRONOUN, CONNECTOR};
+//=================================================
+// File parser.cpp written by Group Number: 03
+//=================================================
+
+// ** Be sure to put the name of the programmer above each function
+// i.e. Done by:
+
+// ** Need syntaxerror1 and syntaxerror2 functions (each takes 2 args)
+
+// ** Need the updated match and next_token (with 2 global vars)
+ 
+// ** Make each non-terminal into a function here
+// ** Be sure to put the corresponding grammar rule above each function
+
+
+//Global
+stack <char> gStack;	//initialize stack, maybe make this a vector for access
+string PTable [3][3];	//parse table, default 3x3 to be changed as needed
 token_type saved_token;			//buffer, default is empty
 bool token_available = false;		//flag, default is unavailable
 fstream toParse;			//global stream to parse file
 
+//token vars
+enum token_type{ //type creation
+	PERIOD, EOFM, VERB, VERBNEG, VERBPASTNEG, 
+	IS, WAS, OBJECT, SUBJECT, DESTINATION,
+	PRONOUN, CONNECTOR};
+
 //General functions
-string scanner();		//not sure where this comes into play yet
+string scanner();	//parser calls this repeatedly
 void next_token();	//go to next token
 bool match(char);	//find and remove from text
 
-//Nonterminal functions, these are something else I think, probably change this
-void V();	//Vowel
-void C();	//Consonants, X on paper
-void R();	//Root
+//Nonterminal functions
+
 
 //Stack functions
-stack <char> gStack;	//initialize grammar stack
 void fillStack();	//fills stack with parse characters
 
 //Syntax error functions
-void syntax_mismatch();		//when match() function does not match
-void syntax_default();		//when a switch statement goes to default
+void syntaxerror1(/*two args here*/);		//when match() function does not match
+void syntaxerror2(/*two args here*/);		//when a switch statement goes to default
 
-int main()
+void fillStack()
 {
-	gStack.push('$');	//push terminating character onto stack
 
-	string PTable [3][3];	//parse table, default 3x3 to be changed as needed
-	string uInput;
-	
-	printf("Please enter the name of the file you'd like to parse: ");
-	getline(cin, uInput);
-
-	text.open(uInput.c_str());		//open test file
-
-	//calls story???
-	scanner(toParse);		//starts scanning for tokens???
-	//scanner calls next_token and match in itself?
-	
-	text.close();			//close file
-
-	return 0;
 }
 
-void scanner(token_type& the_type, string& word)	//grabs next token
+//Done by:
+void syntaxerror1()
 {
-	//W(); called in here?
+
 }
 
+//Done by:
+void syntaxerror2()
+{
+
+}
+
+string scanner(token_type& the_type, string& word)	//grabs next token
+{
+	//parser calls this repeatedly
+}
+
+//Done by: ***
 token next_token()	//she has token here in her notes, part of enum I think
 {
 	string lexeme;	//hold next word
@@ -71,6 +84,7 @@ token next_token()	//she has token here in her notes, part of enum I think
 	return saved_token;	//return found token
 }
 
+//Done by: ***
 bool match(char matchThis)
 {
 	if(next_token() != matchThis)	//mismatch
@@ -92,17 +106,30 @@ bool match(char matchThis)
 	}
 }
 
-void V()	//vowel, could be followed by N
+//Done by: ***
+int main()
 {
+	//- opens the input file
+ 	//- calls the <story> to start parsing
+  	//- closes the input file 
 
-}
+	gStack.push('$');	//push terminating character onto stack
+	
+	string uInput;
+	
+	printf("Please enter the name of the file you'd like to parse: ");
+	getline(cin, uInput);
 
-void C()	//consonant, followed by vowel
-{
+	if(toParse.fail())
+	{
+		//error opening file, end program
+	}
 
-}
+	toParse.open(uInput.c_str());		//open test file
 
-void R()	//root, followed by vowel
-{
+	//calls <story>
+	
+	toParse.close();			//close file
 
+	return 0;
 }
